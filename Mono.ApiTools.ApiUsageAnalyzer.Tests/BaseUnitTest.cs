086ea441d87132d5dc0d8d3cada9cd3daacc08e3
Matthew Leibowitz <mattleibow@live.com>
2024-04-03 20:03:10 +0200

    tests

--- Mono.ApiTools.ApiUsageAnalyzer.Tests/BaseUnitTest.cs
+++ Mono.ApiTools.ApiUsageAnalyzer.Tests/BaseUnitTest.cs
@@ -2,11 +2,17 @@
 
 public class BaseUnitTest
 {
-    public const string LibraryBuiltAgainstV1 = "../../../../TestAssemblies/LibraryBuiltAgainstV1/bin/Debug/net8.0/LibraryBuiltAgainstV1.dll";
+#if DEBUG
+	const string CONFIGURATION = "Debug";
+#else
+	const string CONFIGURATION = "Release";
+#endif
 
-    public const string ClassLibraryV1 = "../../../../TestAssemblies/ClassLibraryV1/bin/Debug/net8.0/ClassLibrary.dll";
-    public const string ClassLibraryV2 = "../../../../TestAssemblies/ClassLibraryV2/bin/Debug/net8.0/ClassLibrary.dll";
+	public const string LibraryBuiltAgainstV1 = "../../../../TestAssemblies/LibraryBuiltAgainstV1/bin/" + CONFIGURATION + "/net8.0/LibraryBuiltAgainstV1.dll";
 
-    public const string SkiaSharpV2Downloader = "../../../../TestAssemblies/SkiaSharpV2Downloader/bin/Debug/net8.0/SkiaSharpV2Downloader.dll";
-    public const string SkiaSharpV3Downloader = "../../../../TestAssemblies/SkiaSharpV3Downloader/bin/Debug/net8.0/SkiaSharpV3Downloader.dll";
+	public const string ClassLibraryV1 = "../../../../TestAssemblies/ClassLibraryV1/bin/" + CONFIGURATION + "/net8.0/ClassLibrary.dll";
+	public const string ClassLibraryV2 = "../../../../TestAssemblies/ClassLibraryV2/bin/" + CONFIGURATION + "/net8.0/ClassLibrary.dll";
+
+	public const string SkiaSharpV2Downloader = "../../../../TestAssemblies/SkiaSharpV2Downloader/bin/" + CONFIGURATION + "/net8.0/SkiaSharpV2Downloader.dll";
+	public const string SkiaSharpV3Downloader = "../../../../TestAssemblies/SkiaSharpV3Downloader/bin/" + CONFIGURATION + "/net8.0/SkiaSharpV3Downloader.dll";
 }
